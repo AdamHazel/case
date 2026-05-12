1f6dbfaf024b3468680296ce508b2c04f5a9e963
Adam Hazel <adamrwhazel@gmail.com>
2026-05-12 18:58:18 +0200

    Removing additional information

--- src/Case.Race/Race.cs
+++ src/Case.Race/Race.cs
@@ -63,7 +63,7 @@ public class Race
             
             Console.WriteLine($"Elapsed time for contestant, driving {contestant.Vehicle.Brand} " +
                               $"({contestant.Vehicle.TypeOfVehicle} - {contestant.Vehicle.GetType().Name} ), " +
-                              $"is {Math.Round(totalTime.TotalHours,1)} hours ({totalTime.Hours}:{totalTime.Minutes}:{totalTime.Seconds})");
+                              $"is {Math.Round(totalTime.TotalHours,1)} hours");
         }
         
         switch (winners.Count)
